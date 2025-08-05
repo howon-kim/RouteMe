b20ca37541ac565fa91aca4be41ac660e291f057
Howon Kim <journal-02-permit@icloud.com>
2025-08-05 11:29:58 -0400

    Initial Commit

---
+++ RouteMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouteMe
+//
+//  Created by Howon Kim on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouteMe/RouteMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteMeApp.swift
+//  RouteMe
+//
+//  Created by Howon Kim on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RouteMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
